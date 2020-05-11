be290f2d8ccaec37cc496a9c4451d9b024c7ecfc
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-10 22:42:31 -0300

    Código similar ao do AVA

--- MatrizesAVA/Form1.Designer.cs
+++ MatrizesAVA/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MatrizesAVA
 {
-    partial class Form1
+    partial class FrmMatriz
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPreenche = new System.Windows.Forms.Button();
+            this.lstMatriz = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnPreenche
+            // 
+            this.btnPreenche.Location = new System.Drawing.Point(13, 13);
+            this.btnPreenche.Name = "btnPreenche";
+            this.btnPreenche.Size = new System.Drawing.Size(120, 23);
+            this.btnPreenche.TabIndex = 0;
+            this.btnPreenche.Text = "Matriz";
+            this.btnPreenche.UseVisualStyleBackColor = true;
+            this.btnPreenche.Click += new System.EventHandler(this.btnPreenche_Click);
+            // 
+            // lstMatriz
+            // 
+            this.lstMatriz.FormattingEnabled = true;
+            this.lstMatriz.Location = new System.Drawing.Point(13, 49);
+            this.lstMatriz.Name = "lstMatriz";
+            this.lstMatriz.Size = new System.Drawing.Size(120, 186);
+            this.lstMatriz.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(303, 258);
+            this.Controls.Add(this.lstMatriz);
+            this.Controls.Add(this.btnPreenche);
+            this.Name = "Form1";
+            this.Text = "Matrizes";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPreenche;
+        private System.Windows.Forms.ListBox lstMatriz;
     }
 }
 
--- MatrizesAVA/Form1.cs
+++ MatrizesAVA/Form1.cs
@@ -10,11 +10,44 @@ using System.Windows.Forms;
 
 namespace MatrizesAVA
 {
-    public partial class Form1 : Form
+    public partial class FrmMatriz : Form
     {
-        public Form1()
+        public FrmMatriz()
         {
             InitializeComponent();
+        }
+
+        private void btnPreenche_Click(object sender, EventArgs e)
+        {
+            lstMatriz.Items.Clear();
+
+            //variáveis para acessar as posições dos índices da matriz
+            // linha - representa linha
+            // coluna - representa coluna
+
+            int linha, coluna;
+
+            //declara a variável numeros com 5 linhas e 4 colunas
+
+            int[,] numeros = new int[5, 4];    //dimensão da matriz 5 x 4
+
+            for (linha = 0; linha < 5; linha++)
+            {
+                for (coluna = 0; coluna < 4; coluna++)
+                {
+                    numeros[linha, coluna] = linha * coluna;  //atribui o valor de linha * coluna
+                }
+            }
+
+            //exibindo os dados no ListBox
+
+            for (linha = 0; linha < 5; linha++)
+            {
+                for (coluna = 0; coluna < 4; coluna++)
+                {
+                    lstMatriz.Items.Add("elemento = " + numeros[linha, coluna]);
+                }
+            }
         }
     }
 }
